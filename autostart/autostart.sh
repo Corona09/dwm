69aa7d351021635f7e9f3a055e1e4157312b9a0c
Corona <Corona09@163.com>
2023-01-18 11:14:03 +0800

    add scripts

---
+++ autostart/autostart.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+picom &
+
+dunst &
+
+feh --bg-fill $HOME/Pictures/wallpaper/wallpaper &
+
+ps -ef -u $USER | grep "/usr/bin/kwalletd5" | grep -v grep | awk '{print $2}' | xargs kill
+nm-applet &
+
+conky &
+
+fcitx5 &
+
+ps -ef -u $USER | grep "/usr/bin/sh $HOME/.local/bin/dwm-bar-repeat" | grep -v grep | awk '{print $2}' | xargs kill
+dwm-bar-repeat &
