121620cf44bbc58209fbac9fa3e027dba78b33af
Corona <Corona09@163.com>
2023-01-18 11:09:11 +0800

    patched alpha-systray

---
+++ sync.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ "$USER" = "root" ]; then
+	echo "Do not execute this script in root/sudo."
+	exit 1
+fi
+
+mkdir -p $HOME/.local/bin
+rm -rf $HOME/.local/bin/dwm*
+cp -f ./scripts/dwm* $HOME/.local/bin/
+chmod +x $HOME/.local/bin/dwm*
+
+rm -rf $HOME/.dwm
+mkdir -p $HOME/.dwm
+cp -f ./autostart/* $HOME/.dwm/
+chmod +x $HOME/.dwm/autostart.sh
