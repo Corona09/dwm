78537131ade813dc392030edd4d1c5b85108ecb2
Corona <Corona09@163.com>
2023-01-20 17:08:13 +0800

    add log output for autostart script

--- autostart/autostart.sh
+++ autostart/autostart.sh
@@ -2,6 +2,11 @@
 
 # 一次性程序
 
+dt=[$(date "+%Y/%m/%d %H:%M:%S")]
+log=$HOME/.dwm-autostart.log
+
+echo -e "$dt\n" >> $log
+
 feh --randomize --bg-fill $HOME/Pictures/wallpaper/
 
 sh $HOME/.dwm/adjust-temp.sh
@@ -20,6 +25,8 @@ conky &
 fcitx5 &
 
 ps -ef -u $USER | grep "dwmblocks" | grep -v grep | awk '{print $2}' | xargs kill 2>/dev/null
-dwmblocks 2>/dev/null &
+dwmblocks 2>>$HOME/.dwmblocks.log &
 
 nextcloud &
+
+echo -e "\n---\n" >> $log
