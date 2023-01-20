e4e643acf991d8834903255f0a40a786c6b8aaa6
Corona <Corona09@163.com>
2023-01-20 15:34:48 +0800

    update dmenu_run command

--- config.h
+++ config.h
@@ -103,7 +103,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, topbar ? NULL : "-b", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, topbar ? NULL : "-b", NULL };
 static const char *dwmlauncher[] = { "dwm-launcher", topbar ? NULL : "-b", NULL };
 static const char *dwmpowermenu[] = { "dwm-powermenu", topbar ? NULL : "-b", NULL };
 static const char *termcmd[]  = { "st", NULL };
